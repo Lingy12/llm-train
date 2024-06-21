7dd04a8edda5c3ff176a081d3b32440c1d5b4e52
lingy <lingy@158-T4-P7-SG-NSCC.cm.cluster>
2024-06-21 11:53:39 +0800

    update training conf

--- run_training.sh
+++ run_training.sh
@@ -1,4 +1,6 @@
-# pip3 install -r requirements.txt
+
+export OMP_NUM_THREADS=26
+
 export NCCL_IB_SL=1
 export NCCL_IB_HCA="mlx5_0:1,mlx5_5:1"
 # export NCCL_DEBUG=INFO
@@ -14,43 +16,57 @@ export WANDB_NAME='test'
 export WANDB_DIR="."
 export WANDB_SERVICE_WAIT=300
 
-lora_trainable="embed_tokens,lm_head,q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
+lr=1e-4
+lora_rank=64
+lora_alpha=128
+lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
 modules_to_save="embed_tokens,lm_head"
+lora_dropout=0.05
 
-echo ===== PRETRAINING =====
-# Node that MODEL_PATH can be local folder path
-MODEL_PATH=./models/Meta-Llama-3-8B
-TITLE=llama-7b-pretrain
-DATA=data/kr
+pretrained_model=./models/Meta-Llama-3-8B
+tokenizer_name_or_path=${pretrained_model}
+dataset_dir=./data/kr
+data_cache=.cache
+per_device_train_batch_size=6
+gradient_accumulation_steps=8
+block_size=1024
+output_dir=outputs
 
-OUTPUT_DIR=outputs/test
-mkdir $OUTPUT_DIR
-
-echo ===== current OUTPUT_DIR is $OUTPUT_DIR =====
-echo ===== MODEL_PATH is $MODEL_PATH =====
-
-torchrun --nproc_per_node=8 --master_port=9919 pretrain.py \
-    --model_name_or_path $MODEL_PATH \
-    --data_path $DATA \
-    --bf16 True \
-    --output_dir $OUTPUT_DIR \
-    --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 4 \
-    --eval_strategy "no" \
-    --save_strategy "steps" \
-    --logging_steps 50 \
-    --save_steps 100 \
-    --save_total_limit 1 \
-    --learning_rate 1e-5 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --use_peft True\
-    --lora_trainable $lora_trainable \
-    --modules_to_save $modules_to_save \
+torchrun --nnodes 1 --nproc_per_node 8 pretrain.py \
+    --model_name_or_path ${pretrained_model} \
+    --tokenizer_name_or_path ${tokenizer_name_or_path} \
+    --dataset_dir ${dataset_dir} \
+    --data_cache_dir ${data_cache} \
+    --validation_split_percentage 0.001 \
+    --per_device_train_batch_size ${per_device_train_batch_size} \
+    --do_train \
+    --low_cpu_mem_usage \
+    --seed $RANDOM \
+    --bf16 \
+    --num_train_epochs 10 \
+    --lr_scheduler_type cosine \
+    --learning_rate ${lr} \
+    --warmup_ratio 0.05 \
+    --weight_decay 0.01 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --save_strategy steps \
+    --save_total_limit 3 \
+    --save_steps 200 \
+    --gradient_accumulation_steps ${gradient_accumulation_steps} \
+    --preprocessing_num_workers 32 \
+    --block_size ${block_size} \
+    --output_dir ${output_dir} \
+    --overwrite_output_dir \
+    --ddp_timeout 30000 \
+    --logging_first_step True \
+    --lora_rank ${lora_rank} \
+    --lora_alpha ${lora_alpha} \
+    --trainable ${lora_trainable} \
+    --lora_dropout ${lora_dropout} \
+    --modules_to_save ${modules_to_save} \
+    --torch_dtype bfloat16 \
     --fsdp "full_shard auto_wrap" \
-    --model_max_length 2048
+    --run_name $WANDB_NAME \
+    --load_in_kbits 16 \
+    --ddp_find_unused_parameters False
